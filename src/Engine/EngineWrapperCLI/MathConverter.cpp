ca64fd5c99a9511515f653f0a2069aa52ac0b751
Diego Tadiotto <tadiotto@gmail.com>
2012-10-03 19:23:42 -0300

    Math conversions

--- src/Engine/EngineWrapperCLI/MathConverter.cpp
+++ src/Engine/EngineWrapperCLI/MathConverter.cpp
@@ -3,7 +3,26 @@
 
 using namespace EngineWrapper;
 
+Vector4 MathConverter::ToUnmanaged(EngineMathCLI::Vector4^ value)
+{
+	return Vector4(value->X, value->Y, value->Z, value->W);
+}
+
 Vector3 MathConverter::ToUnmanaged(EngineMathCLI::Vector3^ value)
 {
 	return Vector3(value->X, value->Y, value->Z);
+}
+
+Vector2 MathConverter::ToUnmanaged(EngineMathCLI::Vector2^ value)
+{
+	return Vector2(value->X, value->Y);
+}
+
+Matrix MathConverter::ToUnmanaged(EngineMathCLI::Matrix^ value)
+{
+	return Matrix(
+		value->M11, value->M12, value->M13, value->M14,
+		value->M21, value->M22, value->M23, value->M24,
+		value->M31, value->M32, value->M33, value->M34,
+		value->M41, value->M42, value->M43, value->M44);
 }
--- src/Engine/EngineWrapperCLI/MathConverter.h
+++ src/Engine/EngineWrapperCLI/MathConverter.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "Vector4.h"
 #include "Vector3.h"
+#include "Vector2.h"
+#include "Matrix.h"
 
 namespace EngineWrapper
 {
 	public ref class MathConverter
 	{
 	public:
+		static Vector4 ToUnmanaged(EngineMathCLI::Vector4^ value);
 		static Vector3 ToUnmanaged(EngineMathCLI::Vector3^ value);
+		static Vector2 ToUnmanaged(EngineMathCLI::Vector2^ value);
+		static Matrix ToUnmanaged(EngineMathCLI::Matrix^ value);
 	};
 }
